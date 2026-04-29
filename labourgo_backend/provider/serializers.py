02d6019d00552108e6cf93f6ed88425b6a72fb28
hamza <hamzabilalbaig@gmail.com>
2026-04-29 22:48:54 +0500

    Removed duplicate backend setup

--- labourgo_backend/provider/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Provider
-
-class ProviderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Provider
-        fields = '__all__'
--- labourgo_backend/provider/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Provider
-from .serializers import ProviderSerializer
-
-class ProviderViewSet(viewsets.ModelViewSet):
-    queryset = Provider.objects.all()
-    serializer_class = ProviderSerializer
